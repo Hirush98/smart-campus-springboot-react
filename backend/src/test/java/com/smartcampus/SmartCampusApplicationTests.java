1407bb9db248d3bb20dd91a891eb6955726fd14e
Hirush98 <161017466+Hirush98@users.noreply.github.com>
2026-04-17 13:54:57 +0530

    TestPropertySource section added

--- backend/src/test/java/com/smartcampus/SmartCampusApplicationTests.java
+++ backend/src/test/java/com/smartcampus/SmartCampusApplicationTests.java
@@ -2,8 +2,13 @@ package com.smartcampus;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "app.jwt.secret=test-secret-key-that-is-long-enough",
+    "app.jwt.expiration-ms=3600000"
+})
 class SmartCampusApplicationTests {
 
     @Test
